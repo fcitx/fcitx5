a808a564929128b03c03c674047db417c14abf84
Weng Xuetian <wengxt@gmail.com>
2016-06-12 22:18:51 -0700

    add missing files

---
+++ src/lib/fcitx/text.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2016~2016 by CSSlayer
+ * wengxt@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sstream>
+#include <vector>
+#include "text.h"
+namespace fcitx {
+
+class TextPrivate {
+public:
+    std::vector<std::pair<std::string, TextFormatFlags>> texts;
+    int cursor = 0;
+};
+
+Text::Text() : d_ptr(std::make_unique<TextPrivate>()) {}
+
+Text::~Text() {}
+
+void Text::clear() {
+    FCITX_D();
+    d->texts.clear();
+    setCursor();
+}
+
+int Text::cursor() const {
+    FCITX_D();
+    return d->cursor;
+}
+
+void Text::setCursor(int pos) {
+    FCITX_D();
+    d->cursor = pos;
+}
+
+void Text::append(const std::string &str, TextFormatFlags flag) {
+    FCITX_D();
+    d->texts.emplace_back(str, flag);
+}
+
+const std::string &Text::stringAt(int idx) const {
+    FCITX_D();
+    return d->texts[idx].first;
+}
+
+TextFormatFlags Text::formatAt(int idx) const {
+    FCITX_D();
+    return d->texts[idx].second;
+}
+
+size_t Text::size() const {
+    FCITX_D();
+    return d->texts.size();
+}
+
+std::string Text::toString() const {
+    FCITX_D();
+    std::stringstream ss;
+    for (auto &p : d->texts) {
+        ss << p.first;
+    }
+
+    return ss.str();
+}
+}
---
+++ src/lib/fcitx/text.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2016~2016 by CSSlayer
+ * wengxt@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FCITX_TEXT_H_
+#define _FCITX_TEXT_H_
+
+#include <string>
+#include <fcitx-utils/flags.h>
+#include <fcitx-utils/macros.h>
+#include <memory>
+#include "fcitxcore_export.h"
+
+namespace fcitx {
+
+enum class TextFormatFlag {
+    UnderLine = (1 << 0), /**< underline is a flag */
+    HighLight = (1 << 1), /**< highlight the preedit */
+    Bold = (1 << 2),
+    Strike = (1 << 3),
+    None = 0,
+};
+
+typedef Flags<TextFormatFlag> TextFormatFlags;
+class TextPrivate;
+class FCITXCORE_EXPORT Text {
+public:
+    Text();
+    virtual ~Text();
+    Text(const Text &other);
+    Text(Text &&other);
+
+    Text &operator=(Text text) {
+        using std::swap;
+        swap(*this, text);
+        return *this;
+    }
+
+    int cursor() const;
+    void setCursor(int pos = -1);
+    void clear();
+    void append(const std::string &str, TextFormatFlags flag = TextFormatFlag::None);
+    const std::string &stringAt(int idx) const;
+    TextFormatFlags formatAt(int idx) const;
+    size_t size() const;
+    std::string toString() const;
+
+private:
+    std::unique_ptr<TextPrivate> d_ptr;
+    FCITX_DECLARE_PRIVATE(Text);
+};
+}
+
+#endif // _FCITX_TEXT_H_
