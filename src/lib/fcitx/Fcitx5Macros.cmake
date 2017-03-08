ec86f40d6c9d2789ecdc3a534db1b0a3e8b3cc47
Weng Xuetian <wengxt@gmail.com>
2017-03-08 10:39:59 -0800

    add new cmake magic for modules

--- src/lib/fcitx/Fcitx5Macros.cmake
+++ src/lib/fcitx/Fcitx5Macros.cmake
@@ -1,6 +1,8 @@
 set(_Fcitx5Macro_SELF "${CMAKE_CURRENT_LIST_FILE}")
 get_filename_component(_Fcitx5Macro_SELF_DIR "${_Fcitx5Macro_SELF}" PATH)
 
+include(WriteBasicConfigVersionFile)
+
 function(fcitx5_download tgt_name url output sha256sum)
   get_filename_component(output "${output}" ABSOLUTE)
   set(FCITX5_DOWNLOAD_URL "${url}")
@@ -44,3 +46,44 @@ function(fcitx5_extract tgt_name ifile)
   add_custom_command(OUTPUT ${FCITX5_EXTRACT_OUTPUT}
     DEPENDS "${tgt_name}")
 endfunction()
+
+
+set(_Fcitx5ModuleMacros_SELF "${CMAKE_CURRENT_LIST_FILE}")
+get_filename_component(_Fcitx5ModuleMacros_SELF_DIR "${_Fcitx5ModuleMacros_SELF}" PATH)
+
+macro(fcitx5_export_module EXPORTNAME)
+    set(options INSTALL)
+    set(one_value_args BUILD_INCLUDE_DIRECTORIES TARGET INCLUDE_INSTALL_DIR LIB_INSTALL_DIR
+                       COMPATIBILITY VERSION)
+    set(multi_value_args HEADERS)
+    cmake_parse_arguments(FEM
+        "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+    if (FEM_INCLUDE_INSTALL_DIR)
+        set(_MODULE_HEADER_DIR "${FEM_INCLUDE_INSTALL_DIR}")
+    else()
+        set(_MODULE_HEADER_DIR "${FCITX_INSTALL_MODULE_HEADER_DIR}/${FEM_TARGET}")
+    endif()
+    if (NOT FEM_LIB_INSTALL_DIR)
+        set(FEM_LIB_INSTALL_DIR "${FCITX_INSTALL_LIBDIR}")
+    endif()
+    set(FEM_EXPORTNAME "${EXPORTNAME}")
+    add_library(${FEM_TARGET}-interface INTERFACE)
+    add_library(Fcitx5::Module::${EXPORTNAME} ALIAS ${FEM_TARGET}-interface)
+    set_target_properties(${FEM_TARGET}-interface PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${FEM_BUILD_INCLUDE_DIRECTORIES}")
+    configure_file(
+        "${_Fcitx5Macro_SELF_DIR}/Fcitx5ModuleTemplate.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/Fcitx5Module${EXPORTNAME}.cmake" @ONLY)
+    configure_file(
+        "${_Fcitx5Macro_SELF_DIR}/Fcitx5ModuleTemplate.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/Fcitx5Module${EXPORTNAME}Config.cmake" @ONLY)
+
+    if (NOT FEM_VERSION)
+        set(FEM_VERSION ${PROJECT_VERSION})
+    endif()
+    write_basic_package_version_file("${CMAKE_CURRENT_BINARY_DIR}/Fcitx5Module${EXPORTNAME}ConfigVersion.cmake" VERSION "${FEM_VERSION}" COMPATIBILITY AnyNewerVersion)
+    if(FEM_INSTALL)
+        install(FILES "${CMAKE_CURRENT_BINARY_DIR}/Fcitx5Module${EXPORTNAME}Config.cmake"
+                      "${CMAKE_CURRENT_BINARY_DIR}/Fcitx5Module${EXPORTNAME}ConfigVersion.cmake" DESTINATION "${FEM_LIB_INSTALL_DIR}/cmake/Fcitx5Module${EXPORTNAME}")
+        install(FILES ${FEM_HEADERS} DESTINATION "${_MODULE_HEADER_DIR}")
+    endif()
+endmacro()
