2920b13beccb2c338828c9d3d756fff24db9d45e
Weng Xuetian <wengxt@gmail.com>
2017-06-13 23:55:59 -0700

    add test for flags;

---
+++ test/testflags.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017~2017 by CSSlayer
+ * wengxt@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+#include "fcitx-utils/flags.h"
+#include <cassert>
+
+using namespace fcitx;
+
+enum class F {
+    F1 = 1,
+    F2 = 2,
+    F3 = 4
+};
+
+int main() {
+    Flags<F> f{F::F1, F::F3};
+    assert(f.test(F::F1));
+    assert(!f.test(F::F2));
+    assert(f.test(F::F3));
+    assert(f.testAny(F::F1));
+    assert(f.testAny(Flags<F>{F::F1, F::F2}));
+    assert(f.testAny(F::F3));
+    return 0;
+}
