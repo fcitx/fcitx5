cd0c7577aea38f6b6e1b89ca06cb932734da594a
Weng Xuetian <wengxt@gmail.com>
2017-10-15 14:30:42 -0700

    use fcitx4 style flag for text format, easier for compatibility.

--- src/lib/fcitx-utils/textformatflags.h
+++ src/lib/fcitx-utils/textformatflags.h
@@ -30,12 +30,12 @@ namespace fcitx {
 
 /// \brief Enum flag for text formatting.
 enum class TextFormatFlag : int {
-    Underline = (1 << 0), /**< underline is a flag */
-    HighLight = (1 << 1), /**< highlight the preedit */
-    Bold = (1 << 2),
-    Strike = (1 << 3),
-    DontCommit = (1 << 4),
-    Italic = (1 << 5),
+    Underline = (1 << 3), /**< underline is a flag */
+    HighLight = (1 << 4), /**< highlight the preedit */
+    DontCommit = (1 << 5),
+    Bold = (1 << 6),
+    Strike = (1 << 7),
+    Italic = (1 << 8),
     None = 0,
 };
 
