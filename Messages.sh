5245938b7f7a2f6a6b115b48cf7ce5463834d3c2
Weng Xuetian <wengxt@gmail.com>
2017-11-22 20:28:43 -0800

    Update scripts.

--- Messages.sh
+++ Messages.sh
@@ -2,10 +2,10 @@
 DOMAIN=$(basename $PWD)
 POT_FILE=po/$DOMAIN.pot
 set -x
-XGETTEXT="xgettext --add-comments --sort-output --msgid-bugs-address=fcitx-dev@googlegroups.com"
+XGETTEXT="xgettext --package-name=$DOMAIN --add-comments --sort-output --msgid-bugs-address=fcitx-dev@googlegroups.com"
 source_files=$(find . -name \*.cpp -o -name \*.h)
 $XGETTEXT --keyword=_ --keyword=N_ --language=C++ -o ${POT_FILE} $source_files
-desktop_files=$(find . -name \*.conf.in -o -name \*.desktop)
+desktop_files=$(find . -name \*.conf.in -o -name \*.conf.in.in -o -name \*.desktop)
 $XGETTEXT --language=Desktop $desktop_files -j -o ${POT_FILE}
 
 sed -i 's|^"Content-Type: text/plain; charset=CHARSET\\n"|"Content-Type: text/plain; charset=utf-8\\n"|g' ${POT_FILE}
@@ -23,7 +23,7 @@ for pofile in $(ls po/*.po | sort); do
   pofilebase=$(basename $pofile)
   pofilebase=${pofilebase/.po/}
   msgmerge -U --backup=none $pofile ${POT_FILE}
-  #project_line=$(grep "Project-Id-Version" ${POT_FILE} | head -n 1 | tr --delete '\n' | sed -e 's/[\/&]/\\&/g')
-  #sed -i "s|.*Project-Id-Version.*|$project_line|g" $pofile
+  project_line=$(grep "Project-Id-Version" ${POT_FILE} | head -n 1 | tr --delete '\n' | sed -e 's/[\/&]/\\&/g')
+  sed -i "s|.*Project-Id-Version.*|$project_line|g" $pofile
   echo $pofilebase >> po/LINGUAS
 done
