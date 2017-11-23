0e7150c38676bd1d983c403c64d4dda9098e61e3
Weng Xuetian <wengxt@gmail.com>
2017-11-22 16:10:54 -0800

    update message.sh and trans

--- Messages.sh
+++ Messages.sh
@@ -2,10 +2,11 @@
 DOMAIN=$(basename $PWD)
 POT_FILE=po/$DOMAIN.pot
 set -x
+XGETTEXT="xgettext --add-comments --sort-output --package-name=${DOMAIN} --msgid-bugs-address=fcitx-dev@googlegroups.com"
 source_files=$(find . -name \*.cpp -o -name \*.h)
-xgettext --keyword=_ --keyword=N_ --language=C++ --add-comments --sort-output -o ${POT_FILE} $source_files
+$XGETTEXT --keyword=_ --keyword=N_ --language=C++ -o ${POT_FILE} $source_files
 desktop_files=$(find . -name \*.conf.in -o -name \*.desktop)
-xgettext --language=Desktop $desktop_files -j -o ${POT_FILE}
+$XGETTEXT --language=Desktop $desktop_files -j -o ${POT_FILE}
 
 echo > po/LINGUAS
 
@@ -13,5 +14,7 @@ for pofile in $(ls po/*.po | sort); do
   pofilebase=$(basename $pofile)
   pofilebase=${pofilebase/.po/}
   msgmerge -U --backup=none $pofile ${POT_FILE}
+  project_line=$(grep "Project-Id-Version" ${POT_FILE} | head -n 1 | tr --delete '\n' | sed -e 's/[\/&]/\\&/g')
+  sed -i "s|.*Project-Id-Version.*|$project_line|g" $pofile
   echo $pofilebase >> po/LINGUAS
 done
