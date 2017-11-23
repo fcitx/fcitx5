99dafdb5a527772677716edc305236a559a6d991
Weng Xuetian <wengxt@gmail.com>
2017-11-22 20:08:59 -0800

    Make po like old fcitx

--- Messages.sh
+++ Messages.sh
@@ -2,16 +2,17 @@
 DOMAIN=$(basename $PWD)
 POT_FILE=po/$DOMAIN.pot
 set -x
-XGETTEXT="xgettext --add-comments --sort-output --package-name=${DOMAIN} --msgid-bugs-address=fcitx-dev@googlegroups.com"
+XGETTEXT="xgettext --add-comments --sort-output --msgid-bugs-address=fcitx-dev@googlegroups.com"
 source_files=$(find . -name \*.cpp -o -name \*.h)
 $XGETTEXT --keyword=_ --keyword=N_ --language=C++ -o ${POT_FILE} $source_files
 desktop_files=$(find . -name \*.conf.in -o -name \*.desktop)
 $XGETTEXT --language=Desktop $desktop_files -j -o ${POT_FILE}
 
-sed -i 's|^"Content-Type: text/plain; charset=CHARSET\\n"|"Content-Type: text/plain; charset=UTF-8\\n"|g' ${POT_FILE}
+sed -i 's|^"Content-Type: text/plain; charset=CHARSET\\n"|"Content-Type: text/plain; charset=utf-8\\n"|g' ${POT_FILE}
 
 # Due to transifex problem, delete the date.
-sed -i '/^"PO-Revision-Date/d' ${POT_FILE}
+#sed -i '/^"PO-Revision-Date/d' ${POT_FILE}
+sed -i 's|^"Language: \\n"|"Language: LANG\\n"|g' ${POT_FILE}
 
 echo > po/LINGUAS
 
