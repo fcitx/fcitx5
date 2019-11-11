e007546c3758f3f578b6b9cef7f7ae15d9ef6052
Weng Xuetian <wengxt@gmail.com>
2019-11-10 22:36:55 -0800

    Abandon clang-format.sh but introduce new .formatignore

--- src/lib/fcitx-utils/dbus/sdbus/objectvtablewrapper.c
+++ src/lib/fcitx-utils/dbus/sdbus/objectvtablewrapper.c
@@ -24,32 +24,36 @@ sd_bus_vtable vtable_start() {
     return result;
 }
 
-sd_bus_vtable vtable_method(const char *member, const char *signature, const char *ret, size_t offset, sd_bus_message_handler_t handler)
-{
-    sd_bus_vtable result = SD_BUS_METHOD_WITH_OFFSET(member, signature, ret, handler, offset, 0);
+sd_bus_vtable vtable_method(const char *member, const char *signature,
+                            const char *ret, size_t offset,
+                            sd_bus_message_handler_t handler) {
+    sd_bus_vtable result =
+        SD_BUS_METHOD_WITH_OFFSET(member, signature, ret, handler, offset, 0);
     return result;
 }
 
-sd_bus_vtable vtable_signal(const char *member, const char *signature)
-{
+sd_bus_vtable vtable_signal(const char *member, const char *signature) {
     sd_bus_vtable result = SD_BUS_SIGNAL(member, signature, 0);
     return result;
 }
 
-sd_bus_vtable vtable_property(const char *member, const char *signature, sd_bus_property_get_t getter, uint32_t flags)
-{
+sd_bus_vtable vtable_property(const char *member, const char *signature,
+                              sd_bus_property_get_t getter, uint32_t flags) {
     sd_bus_vtable result = SD_BUS_PROPERTY(member, signature, getter, 0, flags);
     return result;
 }
 
-sd_bus_vtable vtable_writable_property(const char *member, const char *signature, sd_bus_property_get_t getter, sd_bus_property_set_t setter, uint32_t flags)
-{
-    sd_bus_vtable result = SD_BUS_WRITABLE_PROPERTY(member, signature, getter, setter, 0, flags);
+sd_bus_vtable vtable_writable_property(const char *member,
+                                       const char *signature,
+                                       sd_bus_property_get_t getter,
+                                       sd_bus_property_set_t setter,
+                                       uint32_t flags) {
+    sd_bus_vtable result =
+        SD_BUS_WRITABLE_PROPERTY(member, signature, getter, setter, 0, flags);
     return result;
 }
 
-sd_bus_vtable vtable_end()
-{
+sd_bus_vtable vtable_end() {
     sd_bus_vtable result = SD_BUS_VTABLE_END;
     return result;
 }
