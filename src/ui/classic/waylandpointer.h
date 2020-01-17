a2871df7ecf59ef8c790f38d1677c4741aaf22cd
Weng Xuetian <wengxt@gmail.com>
2020-01-17 15:01:22 -0800

    Fix license header to lgpl

--- src/ui/classic/waylandpointer.h
+++ src/ui/classic/waylandpointer.h
@@ -2,18 +2,19 @@
 // Copyright (C) 2020~2020 by CSSlayer
 // wengxt@gmail.com
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2, or (at your option)
-// any later version.
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; see the file COPYING. If not,
+// see <http://www.gnu.org/licenses/>.
 //
 #ifndef _FCITX_UI_CLASSIC_WAYLANDPOINTER_H_
 #define _FCITX_UI_CLASSIC_WAYLANDPOINTER_H_
