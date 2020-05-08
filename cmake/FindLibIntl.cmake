2cb136c7db5edaa86e243c5da1929d2d4bfa26c8
Weng Xuetian <wengxt@gmail.com>
2020-05-08 10:47:48 -0700

    Fix some cmake warning.

--- cmake/FindLibIntl.cmake
+++ cmake/FindLibIntl.cmake
@@ -1,41 +1,41 @@
 # - find where dlopen and friends are located.
-# LIBINTL_FOUND - system has dynamic linking interface available
-# LIBINTL_INCLUDE_DIR - where dlfcn.h is located.
-# LIBINTL_LIBRARY - libraries needed to use dlopen
+# LibIntl_FOUND - system has dynamic linking interface available
+# LibIntl_INCLUDE_DIR - where dlfcn.h is located.
+# LibIntl_LIBRARY - libraries needed to use dlopen
 
 include(CheckFunctionExists)
 
-find_path(LIBINTL_INCLUDE_DIR NAMES libintl.h)
-find_library(LIBINTL_LIBRARY NAMES intl)
-if(LIBINTL_LIBRARY)
-  set(LIBINTL_FOUND TRUE)
-else(LIBINTL_LIBRARY)
-  check_function_exists(dgettext LIBINTL_FOUND)
+find_path(LibIntl_INCLUDE_DIR NAMES libintl.h)
+find_library(LibIntl_LIBRARY NAMES intl)
+if(LibIntl_LIBRARY)
+  set(LibIntl_FOUND TRUE)
+else(LibIntl_LIBRARY)
+  check_function_exists(dgettext LibIntl_FOUND)
   # If dlopen can be found without linking in dl then dlopen is part
   # of libc, so don't need to link extra libs.
-  set(LIBINTL_LIBRARY "")
-endif(LIBINTL_LIBRARY)
+  set(LibIntl_LIBRARY "")
+endif(LibIntl_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LIBINTL
+find_package_handle_standard_args(LibIntl
     FOUND_VAR
-        LIBINTL_FOUND
+        LibIntl_FOUND
     REQUIRED_VARS
-        LIBINTL_INCLUDE_DIR
+        LibIntl_INCLUDE_DIR
 )
 
-mark_as_advanced(LIBINTL_INCLUDE_DIR LIBINTL_LIBRARY)
+mark_as_advanced(LibIntl_INCLUDE_DIR LibIntl_LIBRARY)
 
-if(LIBINTL_FOUND AND NOT TARGET LibIntl::LibIntl)
-    if (LIBINTL_LIBRARY)
+if(LibIntl_FOUND AND NOT TARGET LibIntl::LibIntl)
+    if (LibIntl_LIBRARY)
         add_library(LibIntl::LibIntl UNKNOWN IMPORTED)
         set_target_properties(LibIntl::LibIntl PROPERTIES
-            IMPORTED_LOCATION "${LIBINTL_LIBRARY}")
+            IMPORTED_LOCATION "${LibIntl_LIBRARY}")
     else()
         add_library(LibIntl::LibIntl INTERFACE IMPORTED )
     endif()
     set_target_properties(LibIntl::LibIntl PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${LIBINTL_INCLUDE_DIR}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LibIntl_INCLUDE_DIR}"
     )
 endif()
 
