e1ddc10fc0d5f3a3ab23891f9cad9b41f7dbc0b4
Weng Xuetian <wengxt@gmail.com>
2021-03-10 22:30:51 -0800

    Fix #241
    Add all missing property from the sni spec. Most of them are just dummy.

--- src/modules/notificationitem/notificationitem.cpp
+++ src/modules/notificationitem/notificationitem.cpp
@@ -114,23 +114,45 @@ public:
     FCITX_OBJECT_VTABLE_SIGNAL(newTitle, "NewTitle", "");
     FCITX_OBJECT_VTABLE_SIGNAL(xayatanaNewLabel, "XAyatanaNewLabel", "ss");
 
-    FCITX_OBJECT_VTABLE_PROPERTY(id, "Id", "s", []() { return "Fcitx"; });
     FCITX_OBJECT_VTABLE_PROPERTY(category, "Category", "s",
                                  []() { return "SystemServices"; });
-    FCITX_OBJECT_VTABLE_PROPERTY(status, "Status", "s",
-                                 []() { return "Active"; });
-    FCITX_OBJECT_VTABLE_PROPERTY(iconName, "IconName", "s",
-                                 [this]() { return iconName(); });
-    FCITX_OBJECT_VTABLE_PROPERTY(attentionIconName, "AttentionIconName", "s",
-                                 []() { return ""; });
+    FCITX_OBJECT_VTABLE_PROPERTY(id, "Id", "s", []() { return "Fcitx"; });
     FCITX_OBJECT_VTABLE_PROPERTY(title, "Title", "s",
                                  []() { return _("Input Method"); });
+    FCITX_OBJECT_VTABLE_PROPERTY(status, "Status", "s",
+                                 []() { return "Active"; });
+    FCITX_OBJECT_VTABLE_PROPERTY(windowId, "WindowId", "u", []() { return 0; });
+    FCITX_OBJECT_VTABLE_PROPERTY(iconName, "IconName", "s",
+                                 [this]() { return iconName(); });
+    FCITX_OBJECT_VTABLE_PROPERTY(
+        iconPixmap, "IconPixmap", "a(iiay)", ([]() {
+            return std::vector<
+                dbus::DBusStruct<int, int, std::vector<uint8_t>>>{};
+        }));
+    FCITX_OBJECT_VTABLE_PROPERTY(overlayIconName, "OverlayIconName", "s",
+                                 ([]() { return ""; }));
+    FCITX_OBJECT_VTABLE_PROPERTY(
+        overlayIconPixmap, "OverlayIconPixmap", "a(iiay)", ([]() {
+            return std::vector<
+                dbus::DBusStruct<int, int, std::vector<uint8_t>>>{};
+        }));
+    FCITX_OBJECT_VTABLE_PROPERTY(attentionIconName, "AttentionIconName", "s",
+                                 []() { return ""; });
+    FCITX_OBJECT_VTABLE_PROPERTY(
+        attentionIconPixmap, "AttentionIconPixmap", "a(iiay)", ([]() {
+            return std::vector<
+                dbus::DBusStruct<int, int, std::vector<uint8_t>>>{};
+        }));
+    FCITX_OBJECT_VTABLE_PROPERTY(attentionMovieName, "AttentionMovieName", "s",
+                                 []() { return ""; });
     FCITX_OBJECT_VTABLE_PROPERTY(tooltip, "ToolTip", "(sa(iiay)ss)",
                                  []() { return tooltip(); });
-    FCITX_OBJECT_VTABLE_PROPERTY(iconThemePath, "IconThemePath", "s",
-                                 []() { return ""; });
+    FCITX_OBJECT_VTABLE_PROPERTY(itemIsMenu, "ItemIsMenu", "b",
+                                 []() { return false; });
     FCITX_OBJECT_VTABLE_PROPERTY(menu, "Menu", "o",
                                  []() { return dbus::ObjectPath("/MenuBar"); });
+    FCITX_OBJECT_VTABLE_PROPERTY(iconThemePath, "IconThemePath", "s",
+                                 []() { return ""; });
     FCITX_OBJECT_VTABLE_PROPERTY(xayatanaLabel, "XAyatanaLabel", "s",
                                  [this]() { return label(); });
     FCITX_OBJECT_VTABLE_PROPERTY(XAyatanaLabelGuide, "XAyatanaLabelGuide", "s",
