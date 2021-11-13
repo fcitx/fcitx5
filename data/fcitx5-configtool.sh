cfd985acb0fd123ddd955b59d80060bd930ed48e
trmdi <trmdi@yandex.com>
2021-11-13 07:14:10 +0700

    Launch configuration inside SystemSettings on KDE (#380)

--- data/fcitx5-configtool.sh
+++ data/fcitx5-configtool.sh
@@ -106,11 +106,11 @@ detectDE() {
 }
 
 run_kde() {
-    if (kcmshell5 --list 2>/dev/null | grep ^kcm_fcitx5 > /dev/null 2>&1); then
+    if (systemsettings5 --list 2>/dev/null | grep ^kcm_fcitx5 > /dev/null 2>&1); then
         if [ x"$1" != x ]; then
-            exec kcmshell5 fcitx5 --args "$1"
+            exec systemsettings5 kcm_fcitx5 --args "$1"
         else
-            exec kcmshell5 fcitx5
+            exec systemsettings5 kcm_fcitx5
         fi
     fi
 }
