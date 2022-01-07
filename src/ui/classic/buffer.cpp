ec0d266c3bf52199c76f6fc73cfb8a420ee706dc
Weng Xuetian <wengxt@gmail.com>
2022-01-06 18:16:34 -0800

    Improve the shm fd creation with modern syscall.

--- src/ui/classic/buffer.cpp
+++ src/ui/classic/buffer.cpp
@@ -14,6 +14,7 @@
 #include <stdexcept>
 #include <vector>
 #include <cairo/cairo.h>
+#include <sys/syscall.h>
 #include <wayland-client.h>
 #include "fcitx-utils/stringutils.h"
 #include "theme.h"
@@ -25,27 +26,100 @@
 
 namespace fcitx::wayland {
 
-Buffer::Buffer(WlShm *shm, uint32_t width, uint32_t height,
-               wl_shm_format format)
-    : width_(width), height_(height) {
+#define RETRY_ON_EINTR(...)                                                    \
+    do {                                                                       \
+        __VA_ARGS__;                                                           \
+    } while (ret < 0 && errno == EINTR)
+
+UnixFD openShm(void) {
+    int ret;
+    // We support multiple different methods, memfd / shm_open on BSD /
+    // O_TMPFILE. While linux has shm_open, it doesn't have SHM_ANON extension
+    // like bsd. Since memfd is introduced in 3.17, which is already quite old,
+    // don't bother to add another implementation for Linux, since we need to
+    // fallback to file as a final resolution.
+#if defined(__NR_memfd_create)
+    do {
+        static bool hasMemfdCreate = true;
+        if (!hasMemfdCreate) {
+            break;
+        }
+        int options = MFD_CLOEXEC;
+#if defined(MFD_ALLOW_SEALING)
+        options |= MFD_ALLOW_SEALING;
+#endif
+        RETRY_ON_EINTR(
+            ret = syscall(__NR_memfd_create, "fcitx-wayland-shm", options));
+
+        if (ret < 0 && errno == ENOSYS) {
+            hasMemfdCreate = false;
+            break;
+        }
+#if defined(F_ADD_SEALS) && defined(F_SEAL_SHRINK)
+        if (ret >= 0) {
+            fcntl(ret, F_ADD_SEALS, F_SEAL_SHRINK);
+            return UnixFD::own(ret);
+        }
+#endif
+    } while (0);
+#endif
+
+    // Try shm_open(SHM_ANON) on BSD.
+#if defined(__FreeBSD__)
+    RETRY_ON_EINTR(
+        ret = shm_open(SHM_ANON, O_CREAT | O_EXCL | O_RDWR | O_CLOEXEC, 0600));
+    if (ret >= 0) {
+        return UnixFD::own(ret);
+    }
+#endif
+
     const char *path = getenv("XDG_RUNTIME_DIR");
     if (!path) {
         throw std::runtime_error("XDG_RUNTIME_DIR is not set");
     }
-    uint32_t stride = width * 4;
-    uint32_t alloc = stride * height;
+
+    // Prefer O_TMPFILE over mkstemp.
+    // The value check is to ensure it has the right value.
+    // It is said that some old glibc may have problem.
+#if defined(O_TMPFILE) && (O_TMPFILE & O_DIRECTORY) == O_DIRECTORY
+    do {
+        std::string pathStr = fs::cleanPath(path);
+        RETRY_ON_EINTR(ret =
+                           open(pathStr.data(),
+                                O_TMPFILE | O_CLOEXEC | O_EXCL | O_RDWR, 0600));
+        if (errno == EOPNOTSUPP || errno == EISDIR) {
+            break;
+        }
+        if (ret >= 0) {
+            return UnixFD::own(ret);
+        }
+        return {};
+    } while (0);
+#endif
+
     auto filename = stringutils::joinPath(path, "fcitx-wayland-shm-XXXXXX");
     std::vector<char> v(filename.begin(), filename.end());
     v.push_back('\0');
-    UnixFD fd = UnixFD::own(mkstemp(v.data()));
+    RETRY_ON_EINTR(ret = mkstemp(v.data()));
+
+    if (ret >= 0) {
+        unlink(v.data());
+        if (int flags = fcntl(ret, F_GETFD); flags != -1) {
+            fcntl(ret, F_SETFD, flags | FD_CLOEXEC);
+        }
+        return UnixFD::own(ret);
+    }
+
+    return {};
+}
+
+Buffer::Buffer(WlShm *shm, uint32_t width, uint32_t height,
+               wl_shm_format format)
+    : width_(width), height_(height) {
+    uint32_t stride = width * 4;
+    uint32_t alloc = stride * height;
+    UnixFD fd = openShm();
     if (!fd.isValid()) {
-        return;
-    }
-    int flags = fcntl(fd.fd(), F_GETFD);
-    if (flags == -1) {
-        return;
-    }
-    if (fcntl(fd.fd(), F_SETFD, flags | FD_CLOEXEC) == -1) {
         return;
     }
 
@@ -54,7 +128,6 @@ Buffer::Buffer(WlShm *shm, uint32_t width, uint32_t height,
     }
     uint8_t *data = (uint8_t *)mmap(nullptr, alloc, PROT_READ | PROT_WRITE,
                                     MAP_SHARED, fd.fd(), 0);
-    unlink(v.data());
 
     if (data == static_cast<uint8_t *>(MAP_FAILED)) {
         return;
