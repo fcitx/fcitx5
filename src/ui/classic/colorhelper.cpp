29b9b32e38118146333f728a10b5b89e4e340199
Weng Xuetian <wengxt@gmail.com>
2023-09-29 13:23:25 -0700

    bump version

--- src/ui/classic/colorhelper.cpp
+++ src/ui/classic/colorhelper.cpp
@@ -1,17 +1,14 @@
+#include "colorhelper.h"
 #include <cmath>
 #include <fcitx-utils/color.h>
-#include "colorhelper.h"
 
 namespace fcitx::classicui {
-    
-inline float normalize(float v)
-{
+
+inline float normalize(float v) {
     return (v < 1.0 ? (v > 0.0 ? v : 0.0) : 1.0);
 }
 
-float gamma(float n) {
-    return std::pow(normalize(n), 2.2);
-}
+float gamma(float n) { return std::pow(normalize(n), 2.2); }
 
 float lumag(float r, float g, float b) {
     return r * 0.2126 + g * 0.7152 + b * 0.0722;
@@ -21,8 +18,7 @@ float luma(const Color &c) {
     return lumag(gamma(c.redF()), gamma(c.greenF()), gamma(c.blueF()));
 }
 
-Color accentForeground(const Color &accent)
-{
+Color accentForeground(const Color &accent) {
     auto c = Color(255, 255, 255);
     // light bg
     if (luma(accent) > 0.5) {
@@ -31,4 +27,4 @@ Color accentForeground(const Color &accent)
     return c;
 }
 
-}
+} // namespace fcitx::classicui
