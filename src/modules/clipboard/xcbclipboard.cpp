a892a91569652cc18653294711b435b5577d9625
Weng Xuetian <wengxt@gmail.com>
2024-04-03 08:47:14 -0700

    Refactor XcbClipboard data fetching and check if data is password (#1003)

--- src/modules/clipboard/xcbclipboard.cpp
+++ src/modules/clipboard/xcbclipboard.cpp
@@ -5,12 +5,121 @@
  *
  */
 #include "xcbclipboard.h"
+#include <string_view>
+#include <xcb/xproto.h>
 #include "clipboard.h"
 
 namespace fcitx {
 
+XcbClipboardData::XcbClipboardData(XcbClipboard *xcbClip, XcbClipboardMode mode)
+    : xcbClip_(xcbClip), mode_(mode) {}
+
+void XcbClipboardData::request() {
+    //
+    callback_.reset();
+    callback_ = xcbClip_->xcb()->call<IXCBModule::convertSelection>(
+        xcbClip_->name(), modeString(), "TARGETS",
+        [this](xcb_atom_t type, const char *data, size_t length) {
+            checkMime(type, data, length);
+        });
+}
+
+const char *XcbClipboardData::modeString() const {
+    if (mode_ == XcbClipboardMode::Primary) {
+        return "PRIMARY";
+    }
+    return "CLIPBOARD";
+}
+
+std::unique_ptr<HandlerTableEntryBase> XcbClipboardData::convertSelection(
+    const char *type, XcbClipboardData::ConvertSelectionFunction fn) {
+    return xcbClip_->xcb()->call<IXCBModule::convertSelection>(
+        xcbClip_->name(), modeString(), type,
+        [this, fn](xcb_atom_t type, const char *data, size_t length) {
+            (this->*fn)(type, data, length);
+        });
+}
+
+bool XcbClipboardData::isValidTextType(xcb_atom_t type) const {
+    if (type == XCB_ATOM_STRING) {
+        return true;
+    }
+    auto utf8Atom = xcbClip_->utf8StringAtom();
+    if (utf8Atom != XCB_ATOM_NONE && type == utf8Atom) {
+        return true;
+    }
+    return false;
+}
+
+void XcbClipboardData::checkMime(xcb_atom_t type, const char *data,
+                                 size_t length) {
+    if (type != XCB_ATOM_ATOM) {
+        callback_.reset();
+        return;
+    }
+
+    const auto *atoms = reinterpret_cast<const xcb_atom_t *>(data);
+    bool maybePassword = false;
+    bool isText = false;
+    size_t size = length / sizeof(xcb_atom_t);
+    for (size_t i = 0; i < size; i++) {
+        if (xcbClip_->passwordAtom() != XCB_ATOM_NONE &&
+            atoms[i] == xcbClip_->passwordAtom()) {
+            maybePassword = true;
+        } else if (isValidTextType(atoms[i])) {
+            isText = true;
+        }
+    }
+
+    if (!isText) {
+        callback_.reset();
+        return;
+    }
+
+    if (maybePassword) {
+        callback_ = convertSelection(PASSWORD_MIME_TYPE,
+                                     &XcbClipboardData::checkPassword);
+    } else {
+        callback_ = convertSelection("", &XcbClipboardData::readData);
+    }
+}
+
+void XcbClipboardData::checkPassword(xcb_atom_t /*type*/, const char *data,
+                                     size_t length) {
+    std::string_view str(data, length);
+    if (str == "secret") {
+        callback_.reset();
+        return;
+    }
+    callback_ = convertSelection("", &XcbClipboardData::readData);
+}
+
+void XcbClipboardData::readData(xcb_atom_t type, const char *data,
+                                size_t length) {
+
+    switch (mode_) {
+    case XcbClipboardMode::Primary:
+        if (!data || !isValidTextType(type)) {
+            xcbClip_->setPrimary("");
+        } else {
+            std::string str(data, length);
+            xcbClip_->setPrimary(str);
+        }
+        break;
+    case XcbClipboardMode::Clipboard:
+        if (isValidTextType(type) && data) {
+            std::string str(data, length);
+            xcbClip_->setClipboard(str);
+        }
+        break;
+    }
+    callback_.reset();
+}
+
 XcbClipboard::XcbClipboard(Clipboard *clipboard, std::string name)
-    : parent_(clipboard), name_(std::move(name)), xcb_(clipboard->xcb()) {
+    : parent_(clipboard), name_(std::move(name)), xcb_(clipboard->xcb()),
+      primary_(this, XcbClipboardMode::Primary),
+      clipboard_(this, XcbClipboardMode::Clipboard) {
 
     // Ensure that atom exists. See:
     // https://github.com/fcitx/fcitx5/issues/610 PRIMARY /
@@ -20,6 +129,8 @@ XcbClipboard::XcbClipboard(Clipboard *clipboard, std::string name)
     xcb_->call<IXCBModule::atom>(name_, "PRIMARY", false);
     xcb_->call<IXCBModule::atom>(name_, "CLIPBOARD", false);
     xcb_->call<IXCBModule::atom>(name_, "TARGETS", false);
+    passwordAtom_ =
+        xcb_->call<IXCBModule::atom>(name_, PASSWORD_MIME_TYPE, false);
     utf8StringAtom_ = xcb_->call<IXCBModule::atom>(name_, "UTF8_STRING", false);
     selectionCallbacks_.emplace_back(xcb_->call<IXCBModule::addSelection>(
         name_, "PRIMARY", [this](xcb_atom_t) { primaryChanged(); }));
@@ -29,38 +140,9 @@ XcbClipboard::XcbClipboard(Clipboard *clipboard, std::string name)
     clipboardChanged();
 }
 
-void XcbClipboard::primaryChanged() {
-    primaryCallback_ = xcb_->call<IXCBModule::convertSelection>(
-        name_, "PRIMARY", "",
-        [this](xcb_atom_t type, const char *data, size_t length) {
-            if (type != XCB_ATOM_STRING && type != utf8StringAtom_) {
-                return;
-            }
-            if (!data) {
-                parent_->setPrimary(name_, "");
-            } else {
-                std::string str(data, length);
-                parent_->setPrimary(name_, str);
-            }
-            primaryCallback_.reset();
-        });
-}
+void XcbClipboard::primaryChanged() { primary_.request(); }
 
-void XcbClipboard::clipboardChanged() {
-    clipboardCallback_ = xcb_->call<IXCBModule::convertSelection>(
-        name_, "CLIPBOARD", "",
-        [this](xcb_atom_t type, const char *data, size_t length) {
-            if (type != XCB_ATOM_STRING && type != utf8StringAtom_) {
-                return;
-            }
-            if (!data || !length) {
-                return;
-            }
-            std::string str(data, length);
-            parent_->setClipboard(name_, str);
-            clipboardCallback_.reset();
-        });
-}
+void XcbClipboard::clipboardChanged() { clipboard_.request(); }
 
 void XcbClipboard::setClipboard(const std::string &str) {
     parent_->setClipboard(name_, str);
