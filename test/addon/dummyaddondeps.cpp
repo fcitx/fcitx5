ee2fd466413e9a96c992d1fbbbd3e8e8b8df7a29
Weng Xuetian <wengxt@gmail.com>
2024-12-03 07:50:27 -0800

    Add support for shared addon loader to load multiple libraries (#1195)
    In general this is not very useful on linux, but for android where
    LD_LIBRARY_PATH doesn't work, it can be used as an built-in linker to
    load depenedency libraries.

---
+++ test/addon/dummyaddondeps.cpp
@@ -0,0 +1,8 @@
+#include <fcitx-utils/log.h>
+
+FCITX_DEFINE_LOG_CATEGORY(dummyaddondeps, "dummyaddondeps");
+
+static int init = []() {
+    fcitx::Log::setLogRule("default=3");
+    return 0;
+}();
