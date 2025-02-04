fcitx/fcitx5

commit e723881adf82e7eafa19dfc635eb929cdffc87e0

Clang-tidy clean up for classic ui

src/ui/classic/classicui.cpp
@@ -7,24 +7,49 @@
 
 #include "classicui.h"
 #include <fcntl.h>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <memory>
 #include <optional>
+#include <set>
+#include <stdexcept>
 #include <string>
 #include <string_view>
+#include <utility>
+#include <vector>
+#include <cairo.h>
+#include <xcb/xcb.h>
 #include "fcitx-config/iniparser.h"
+#include "fcitx-config/rawconfig.h"
+#include "fcitx-utils/color.h"
 #include "fcitx-utils/dbus/message_details.h"
+#include "fcitx-utils/eventloopinterface.h"
+#include "fcitx-utils/fs.h"
+#include "fcitx-utils/i18n.h"
+#include "fcitx-utils/log.h"
+#include "fcitx-utils/misc.h"
 #include "fcitx-utils/misc_p.h"
 #include "fcitx-utils/standardpath.h"
+#include "fcitx-utils/stringutils.h"
+#include "fcitx/addonfactory.h"
+#include "fcitx/addoninstance.h"
 #include "fcitx/event.h"
 #include "fcitx/inputcontext.h"
 #include "fcitx/inputcontextmanager.h"
 #include "fcitx/instance.h"
+#include "fcitx/userinterface.h"
 #include "common.h"
 #include "notificationitem_public.h"
 #include "plasmathemewatchdog.h"
+#include "theme.h"
 #ifdef ENABLE_X11
+#include "xcb_public.h"
 #include "xcbui.h"
 #endif
 #ifdef WAYLAND_FOUND
+#include "wayland_public.h"
 #include "waylandui.h"
 #endif
 #ifdef ENABLE_DBUS
@@ -40,9 +65,8 @@ FCITX_DEFINE_LOG_CATEGORY(classicui_logcategory, "classicui");
 using AccentColorDBusType = FCITX_STRING_TO_DBUS_TYPE("(ddd)");
 
 ClassicUI::ClassicUI(Instance *instance) : instance_(instance) {
-
 #ifdef ENABLE_DBUS
-    if (auto dbusAddon = dbus()) {
+    if (auto *dbusAddon = dbus()) {
         dbus::VariantTypeRegistry::defaultRegistry()
             .registerType<AccentColorDBusType>();
         settingMonitor_ = std::make_unique<PortalSettingMonitor>(
@@ -108,7 +132,7 @@ ClassicUI::ClassicUI(Instance *instance) : instance_(instance) {
             auto &focusEvent =
                 static_cast<FocusGroupFocusChangedEvent &>(event);
             if (!focusEvent.newFocus()) {
-                if (auto ui = uiForDisplay(focusEvent.group()->display())) {
+                if (auto *ui = uiForDisplay(focusEvent.group()->display())) {
                     ui->update(UserInterfaceComponent::InputPanel, nullptr);
                 }
             }
@@ -400,7 +424,7 @@ void ClassicUI::resume() {
             auto &focusEvent =
                 static_cast<FocusGroupFocusChangedEvent &>(event);
             if (!focusEvent.newFocus()) {
-                if (auto ui = uiForDisplay(focusEvent.group()->display())) {
+                if (auto *ui = uiForDisplay(focusEvent.group()->display())) {
                     ui->update(UserInterfaceComponent::InputPanel, nullptr);
                 }
             }

src/ui/classic/buffer.cpp
@@ -6,15 +6,20 @@
  */
 #include "buffer.h"
 #include <fcntl.h>
-#include <stdlib.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <cerrno>
+#include <cstdint>
+#include <cstdlib>
 #include <stdexcept>
+#include <string>
 #include <vector>
 #include <cairo.h>
 #include <sys/syscall.h>
+#include <wayland-client.h>
+#include "fcitx-utils/fs.h"
 #include "fcitx-utils/stringutils.h"
-#include "theme.h"
+#include "fcitx-utils/unixfd.h"
 #include "wl_buffer.h"
 #include "wl_callback.h"
 #include "wl_shm.h"
@@ -28,7 +33,7 @@ namespace fcitx::wayland {
         __VA_ARGS__;                                                           \
     } while (ret < 0 && errno == EINTR)
 
-UnixFD openShm(void) {
+UnixFD openShm() {
     int ret;
     // We support multiple different methods, memfd / shm_open on BSD /
     // O_TMPFILE. While linux has shm_open, it doesn't have SHM_ANON extension

src/ui/classic/buffer.h
@@ -7,10 +7,12 @@
 #ifndef _FCITX_WAYLAND_CORE_BUFFER_H_
 #define _FCITX_WAYLAND_CORE_BUFFER_H_
 
+#include <cstddef>
 #include <cstdint>
 #include <memory>
-#include <cairo/cairo.h>
+#include <cairo.h>
 #include <wayland-client.h>
+#include "fcitx-utils/misc.h"
 #include "fcitx-utils/signals.h"
 
 namespace fcitx {

src/ui/classic/classicui.h
@@ -7,19 +7,30 @@
 #ifndef _FCITX_UI_CLASSIC_CLASSICUI_H_
 #define _FCITX_UI_CLASSIC_CLASSICUI_H_
 
+#include <cstddef>
 #include <memory>
-#include "config.h"
-
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 #include "fcitx-config/configuration.h"
 #include "fcitx-config/iniparser.h"
 #include "fcitx-config/option.h"
+#include "fcitx-config/rawconfig.h"
+#include "fcitx-utils/color.h"
+#include "fcitx-utils/eventloopinterface.h"
+#include "fcitx-utils/handlertable.h"
 #include "fcitx-utils/i18n.h"
-#include "fcitx/addonfactory.h"
+#include "fcitx-utils/stringutils.h"
 #include "fcitx/addoninstance.h"
 #include "fcitx/addonmanager.h"
+#include "fcitx/event.h"
 #include "fcitx/instance.h"
 #include "fcitx/userinterface.h"
 #include "classicui_public.h"
+#include "config.h"
 #include "plasmathemewatchdog.h"
 #include "portalsettingmonitor.h"
 #include "theme.h"
@@ -30,11 +41,9 @@
 #include "wayland_public.h"
 #endif
 #ifdef ENABLE_DBUS
-#include "fcitx-utils/dbus/bus.h"
 #endif
 
-namespace fcitx {
-namespace classicui {
+namespace fcitx::classicui {
 
 inline constexpr std::string_view PlasmaThemeName = "plasma";
 
@@ -47,8 +56,8 @@ public:
     virtual ~UIInterface() {}
     virtual void update(UserInterfaceComponent component,
                         InputContext *inputContext) = 0;
-    virtual void updateCursor(InputContext *) {}
-    virtual void updateCurrentInputMethod(InputContext *) {}
+    virtual void updateCursor(InputContext * /*unused*/) {}
+    virtual void updateCurrentInputMethod(InputContext * /*unused*/) {}
     virtual void suspend() = 0;
     virtual void resume() {}
     virtual void setEnableTray(bool) = 0;
@@ -59,7 +68,7 @@ private:
 
 struct NotEmpty {
     bool check(const std::string &value) { return !value.empty(); }
-    void dumpDescription(RawConfig &) const {}
+    void dumpDescription(RawConfig & /*unused*/) const {}
 };
 
 struct ThemeAnnotation : public EnumAnnotation {
@@ -274,7 +283,6 @@ private:
     std::unique_ptr<EventSource> deferedEnableTray_;
     std::unique_ptr<PlasmaThemeWatchdog> plasmaThemeWatchdog_;
 };
-} // namespace classicui
-} // namespace fcitx
+} // namespace fcitx::classicui
 
 #endif // _FCITX_UI_CLASSIC_CLASSICUI_H_

src/ui/classic/common.h
@@ -7,12 +7,11 @@
 #ifndef _FCITX5_UI_CLASSIC_COMMON_H_
 #define _FCITX5_UI_CLASSIC_COMMON_H_
 
-#include <memory>
 #include <glib-object.h>
 #include "fcitx-utils/log.h"
+#include "fcitx-utils/misc.h"
 
-namespace fcitx {
-namespace classicui {
+namespace fcitx::classicui {
 
 template <typename T>
 using GObjectUniquePtr = UniqueCPtr<T, g_object_unref>;
@@ -25,7 +24,6 @@ FCITX_DECLARE_LOG_CATEGORY(classicui_logcategory);
 #define CLASSICUI_INFO()                                                       \
     FCITX_LOGC(::fcitx::classicui::classicui_logcategory, Info)
 
-} // namespace classicui
-} // namespace fcitx
+} // namespace fcitx::classicui
 
 #endif // _FCITX5_UI_CLASSIC_COMMON_H_

src/ui/classic/inputwindow.cpp
@@ -5,21 +5,38 @@
  *
  */
 #include "inputwindow.h"
+#include <algorithm>
 #include <cmath>
+#include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <initializer_list>
 #include <limits>
+#include <string>
+#include <tuple>
+#include <utility>
 #include <cairo.h>
+#include <pango/pango-attributes.h>
 #include <pango/pango-context.h>
 #include <pango/pango-font.h>
+#include <pango/pango-fontmap.h>
+#include <pango/pango-language.h>
 #include <pango/pango-layout.h>
+#include <pango/pango-matrix.h>
+#include <pango/pango-types.h>
 #include <pango/pangocairo.h>
 #include "fcitx-utils/color.h"
+#include "fcitx-utils/rect.h"
+#include "fcitx-utils/textformatflags.h"
+#include "fcitx/candidatelist.h"
 #include "fcitx/inputmethodentry.h"
 #include "fcitx/inputpanel.h"
 #include "fcitx/instance.h"
 #include "fcitx/misc_p.h"
+#include "fcitx/text.h"
+#include "fcitx/userinterface.h"
 #include "classicui.h"
+#include "common.h"
 #include "theme.h"
 
 namespace fcitx::classicui {
@@ -46,7 +63,7 @@ static void prepareLayout(cairo_t *cr, PangoLayout *layout) {
 }
 
 static void renderLayout(cairo_t *cr, PangoLayout *layout, int x, int y) {
-    auto context = pango_layout_get_context(layout);
+    auto *context = pango_layout_get_context(layout);
     auto *metrics = pango_context_get_metrics(
         context, pango_context_get_font_description(context),
         pango_context_get_language(context));
@@ -58,7 +75,10 @@ static void renderLayout(cairo_t *cr, PangoLayout *layout, int x, int y) {
 
     // Ensure the text are not painting on half pixel.
     cairo_move_to(cr, x, y + yOffset);
-    double dx, dy, odx, ody;
+    double dx;
+    double dy;
+    double odx;
+    double ody;
     cairo_get_current_point(cr, &dx, &dy);
     // Save old user value
     odx = dx;
@@ -80,7 +100,8 @@ static void renderLayout(cairo_t *cr, PangoLayout *layout, int x, int y) {
 int MultilineLayout::width() const {
     int width = 0;
     for (const auto &layout : lines_) {
-        int w, h;
+        int w;
+        int h;
         pango_layout_get_pixel_size(layout.get(), &w, &h);
         width = std::max(width, w);
     }
@@ -245,19 +266,19 @@ void InputWindow::setTextToLayout(
         appendText(line, newAttrList, newHighlightAttrList, text);
     }
 
-    auto entry = parent_->instance()->inputMethodEntry(inputContext);
+    const auto *entry = parent_->instance()->inputMethodEntry(inputContext);
     if (*parent_->config().useInputMethodLanguageToDisplayText && entry &&
         !entry->languageCode().empty()) {
-        if (auto language =
+        if (auto *language =
                 pango_language_from_string(entry->languageCode().c_str())) {
             if (newAttrList) {
-                auto attr = pango_attr_language_new(language);
+                auto *attr = pango_attr_language_new(language);
                 attr->start_index = 0;
                 attr->end_index = line.size();
                 pango_attr_list_insert(newAttrList, attr);
             }
             if (newHighlightAttrList) {
-                auto attr = pango_attr_language_new(language);
+                auto *attr = pango_attr_language_new(language);
                 attr->start_index = 0;
                 attr->end_index = line.size();
                 pango_attr_list_insert(newHighlightAttrList, attr);
@@ -364,7 +385,8 @@ std::pair<int, int> InputWindow::update(InputContext *inputContext) {
     visible_ = nCandidates_ ||
                pango_layout_get_character_count(upperLayout_.get()) ||
                pango_layout_get_character_count(lowerLayout_.get());
-    int width = 0, height = 0;
+    int width = 0;
+    int height = 0;
     if (visible_) {
         std::tie(width, height) = sizeHint();
         if (width <= 0 || height <= 0) {
@@ -402,7 +424,8 @@ std::pair<unsigned int, unsigned int> InputWindow::sizeHint() {
             m = n;
         }
     };
-    int w, h;
+    int w;
+    int h;
 
     const auto &textMargin = *theme.inputPanel->textMargin;
     auto extraW = *textMargin.marginLeft + *textMargin.marginRight;
@@ -425,9 +448,11 @@ std::pair<unsigned int, unsigned int> InputWindow::sizeHint() {
         vertical = false;
     }
 
-    size_t wholeH = 0, wholeW = 0;
+    size_t wholeH = 0;
+    size_t wholeW = 0;
     for (size_t i = 0; i < nCandidates_; i++) {
-        size_t candidateW = 0, candidateH = 0;
+        size_t candidateW = 0;
+        size_t candidateH = 0;
         if (labelLayouts_[i].characterCount()) {
             candidateW += labelLayouts_[i].width();
             updateIfLarger(candidateH,
@@ -493,7 +518,8 @@ void InputWindow::paint(cairo_t *cr, unsigned int width, unsigned int height,
     fontHeight = PANGO_PIXELS(fontHeight);
 
     size_t currentHeight = 0;
-    int w, h;
+    int w;
+    int h;
     auto extraW = *textMargin.marginLeft + *textMargin.marginRight;
     auto extraH = *textMargin.marginTop + *textMargin.marginBottom;
     if (pango_layout_get_character_count(upperLayout_.get())) {
@@ -533,14 +559,16 @@ void InputWindow::paint(cairo_t *cr, unsigned int width, unsigned int height,
 
     candidateRegions_.clear();
     candidateRegions_.reserve(nCandidates_);
-    size_t wholeW = 0, wholeH = 0;
+    size_t wholeW = 0;
+    size_t wholeH = 0;
 
     // size of text = textMargin + actual text size.
     // HighLight = HighLight margin + TEXT.
     // Click region = HighLight - click
 
     for (size_t i = 0; i < nCandidates_; i++) {
-        int x, y;
+        int x;
+        int y;
         if (vertical) {
             x = 0;
             y = currentHeight + wholeH;
@@ -550,7 +578,10 @@ void InputWindow::paint(cairo_t *cr, unsigned int width, unsigned int height,
         }
         x += *textMargin.marginLeft;
         y += *textMargin.marginTop;
-        int labelW = 0, labelH = 0, candidateW = 0, candidateH = 0;
+        int labelW = 0;
+        int labelH = 0;
+        int candidateW = 0;
+        int candidateH = 0;
         if (labelLayouts_[i].characterCount()) {
             labelW = labelLayouts_[i].width();
             labelH = fontHeight * labelLayouts_[i].size();
@@ -620,7 +651,8 @@ void InputWindow::paint(cairo_t *cr, unsigned int width, unsigned int height,
         const auto &next = theme.loadAction(*theme.inputPanel->next);
         if (prev.valid() && next.valid()) {
             cairo_save(cr);
-            int prevY = 0, nextY = 0;
+            int prevY = 0;
+            int nextY = 0;
             switch (*theme.inputPanel->buttonAlignment) {
             case PageButtonAlignment::Top:
                 prevY = *margin.marginTop;

src/ui/classic/inputwindow.h
@@ -7,15 +7,28 @@
 #ifndef _FCITX_UI_CLASSIC_INPUTWINDOW_H_
 #define _FCITX_UI_CLASSIC_INPUTWINDOW_H_
 
+#include <cstddef>
+#include <functional>
+#include <initializer_list>
+#include <string>
 #include <utility>
-#include <cairo/cairo.h>
+#include <vector>
+#include <cairo.h>
+#include <pango/pango-attributes.h>
+#include <pango/pango-layout.h>
+#include <pango/pango-types.h>
 #include <pango/pango.h>
+#include "fcitx-utils/macros.h"
+#include "fcitx-utils/misc.h"
+#include "fcitx-utils/rect.h"
+#include "fcitx-utils/textformatflags.h"
+#include "fcitx-utils/trackableobject.h"
 #include "fcitx/candidatelist.h"
 #include "fcitx/inputcontext.h"
+#include "fcitx/text.h"
 #include "common.h"
 
-namespace fcitx {
-namespace classicui {
+namespace fcitx::classicui {
 
 class ClassicUI;
 
@@ -41,7 +54,7 @@ public:
 
     int width() const;
 
-    int size() { return lines_.size(); }
+    int size() const { return lines_.size(); }
     void render(cairo_t *cr, int x, int y, int lineHeight, bool highlight);
 
     std::vector<GObjectUniquePtr<PangoLayout>> lines_;
@@ -107,7 +120,6 @@ private:
     std::pair<unsigned int, unsigned int> sizeHint();
 };
 
-} // namespace classicui
-} // namespace fcitx
+} // namespace fcitx::classicui
 
 #endif // _FCITX_UI_CLASSIC_INPUTWINDOW_H_

src/ui/classic/plasmathemewatchdog.cpp
@@ -18,6 +18,7 @@
 #include <string>
 #include <utility>
 #include "fcitx-utils/event.h"
+#include "fcitx-utils/eventloopinterface.h"
 #include "fcitx-utils/fs.h"
 #include "fcitx-utils/standardpath.h"
 #include "fcitx-utils/unixfd.h"

commit 8dc3dc8beda508670b2a5934d2bd872419291f4b

ui: classic: Include xcb/xcb.h only when X11 is enabled
The header may be not available when X11 is disabled. Fixes fcitx5
build without X11 libraries.

Fixes: e723881a ("Clang-tidy clean up for classic ui")
Signed-off-by: Yao Zi <ziyao@disroot.org>

src/ui/classic/classicui.cpp
@@ -20,7 +20,6 @@
 #include <utility>
 #include <vector>
 #include <cairo.h>
-#include <xcb/xcb.h>
 #include "fcitx-config/iniparser.h"
 #include "fcitx-config/rawconfig.h"
 #include "fcitx-utils/color.h"
@@ -45,6 +44,7 @@
 #include "plasmathemewatchdog.h"
 #include "theme.h"
 #ifdef ENABLE_X11
+#include <xcb/xcb.h>
 #include "xcb_public.h"
 #include "xcbui.h"
 #endif