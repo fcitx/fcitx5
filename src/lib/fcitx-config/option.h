1fa0aeba6a7d8d294bf3133686e77a3d84dbc292
Weng Xuetian <wengxt@gmail.com>
2025-07-16 07:46:07 -0700

    New option constructor with designated initializer.

--- src/lib/fcitx-config/option.h
+++ src/lib/fcitx-config/option.h
@@ -281,6 +281,18 @@ private:
     value_type value_;
 };
 
+template <typename Constrain, typename Marshaller, typename Annotation,
+          typename T>
+struct OptionParameters {
+    Configuration *parent;
+    std::string path;
+    std::string description;
+    T defaultValue;
+    Constrain constrain{};
+    Marshaller marshaller{};
+    Annotation annotation{};
+};
+
 /**
  * Represent a Configuration option.
  *
@@ -292,6 +304,8 @@ class Option : public OptionBaseV3 {
 public:
     using value_type = T;
     using constrain_type = Constrain;
+    using OptionParametersType =
+        OptionParameters<Constrain, Marshaller, Annotation, T>;
 
     Option(Configuration *parent, std::string path, std::string description,
            const T &defaultValue = T(), Constrain constrain = Constrain(),
@@ -306,6 +320,12 @@ public:
                 "defaultValue doesn't satisfy constrain");
         }
     }
+
+    Option(OptionParametersType params)
+        : Option(params.parent, std::move(params.path),
+                 std::move(params.description), std::move(params.defaultValue),
+                 std::move(params.constrain), std::move(params.marshaller),
+                 std::move(params.annotation)) {}
 
     std::string typeString() const override { return OptionTypeName<T>::get(); }
 
@@ -421,9 +441,14 @@ template <typename T, typename Annotation>
 using OptionWithAnnotation =
     Option<T, NoConstrain<T>, DefaultMarshaller<T>, Annotation>;
 
+/// Shorthand if you want a option type with only custom annotation.
+template <typename Annotation>
+using KeyListOptionWithAnnotation =
+    Option<KeyList, ListConstrain<KeyConstrain>, DefaultMarshaller<KeyList>,
+           Annotation>;
+
 /// Shorthand for KeyList option with constrain.
-using KeyListOption = Option<KeyList, ListConstrain<KeyConstrain>,
-                             DefaultMarshaller<KeyList>, NoAnnotation>;
+using KeyListOption = KeyListOptionWithAnnotation<NoAnnotation>;
 
 /// Shorthand for create a key list constrain.
 static inline ListConstrain<KeyConstrain>
@@ -465,7 +490,7 @@ struct ConditionalHiddenHelper<true, SubConfigOption> {
     class HiddenSubConfigOption : public SubConfigOption {
     public:
         using SubConfigOption::SubConfigOption;
-        bool skipDescription() const { return true; }
+        bool skipDescription() const override { return true; }
     };
     using OptionType = HiddenSubConfigOption;
 };
