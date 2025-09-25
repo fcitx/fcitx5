a9599327a78d68a4b21598caf9b5f2aeac250f23
Weng Xuetian <wengxt@gmail.com>
2025-09-25 01:41:13 -0700

    Add missing "-k" option in command line

--- src/lib/fcitx/instance.cpp
+++ src/lib/fcitx/instance.cpp
@@ -1286,7 +1286,7 @@ void InstanceArgument::parseOption(int argc, char **argv) {
     int optionIndex = 0;
     int c;
     std::string addonOptionString;
-    while ((c = getopt_long(argc, argv, "ru:dDs:hvo:", longOptions,
+    while ((c = getopt_long(argc, argv, "ru:dDs:hvo:k", longOptions,
                             &optionIndex)) != EOF) {
         switch (c) {
         case 0: {
