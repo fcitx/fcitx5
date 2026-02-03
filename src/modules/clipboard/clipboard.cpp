6f1df16e407718c170b68cf64eb59e0cfae2cf4a
Sven Püschel <SvenPschel@akarisu.de>
2026-02-03 22:50:13 +0100

    clipboard: implement search functionality
    Implement a search functionality to allow searching for previous
    clipboard entries. This enables the user to search for a given entry.
    It works similar to the unicode module, which allows searching for a
    given unicode symbol.

--- src/modules/clipboard/clipboard.cpp
+++ src/modules/clipboard/clipboard.cpp
@@ -18,6 +18,7 @@
 #include "fcitx-utils/event.h"
 #include "fcitx-utils/eventloopinterface.h"
 #include "fcitx-utils/i18n.h"
+#include "fcitx-utils/inputbuffer.h"
 #include "fcitx-utils/key.h"
 #include "fcitx-utils/keysym.h"
 #include "fcitx-utils/log.h"
@@ -70,13 +71,16 @@ FCITX_DEFINE_LOG_CATEGORY(clipboard_log, "clipboard");
 
 class ClipboardState : public InputContextProperty {
 public:
-    ClipboardState(Clipboard *q) : q_(q) {}
+    ClipboardState(Clipboard *q) : q_(q) { buffer_.setMaxSize(30); }
 
     bool enabled_ = false;
     Clipboard *q_;
+    InputBuffer buffer_;
 
     void reset(InputContext *ic) {
         enabled_ = false;
+        buffer_.clear();
+        buffer_.shrinkToFit();
         ic->inputPanel().reset();
         ic->updatePreedit();
         ic->updateUserInterface(UserInterfaceComponent::InputPanel);
@@ -264,8 +268,7 @@ Clipboard::Clipboard(Instance *instance)
                     }
                     return;
                 }
-                if (keyEvent.key().check(FcitxKey_space) ||
-                    keyEvent.key().check(FcitxKey_Return) ||
+                if (keyEvent.key().check(FcitxKey_Return) ||
                     keyEvent.key().check(FcitxKey_KP_Enter)) {
                     keyEvent.accept();
                     if (!candidateList->empty() &&
@@ -331,12 +334,44 @@ Clipboard::Clipboard(Instance *instance)
                 return;
             }
             if (keyEvent.key().check(FcitxKey_Delete) ||
-                keyEvent.key().check(FcitxKey_BackSpace)) {
+                (keyEvent.key().check(FcitxKey_BackSpace) &&
+                 state->buffer_.empty())) {
                 keyEvent.accept();
                 history_.clear();
                 primary_.clear();
                 state->reset(inputContext);
                 return;
+            }
+
+            if (keyEvent.key().check(FcitxKey_BackSpace)) {
+                event.accept();
+                if (state->buffer_.empty()) {
+                    state->reset(inputContext);
+                } else {
+                    if (state->buffer_.backspace()) {
+                        if (state->buffer_.empty()) {
+                            state->reset(inputContext);
+                        } else {
+                            updateUI(inputContext);
+                        }
+                    }
+                }
+                return;
+            }
+
+            // check compose first.
+            auto compose = instance_->processComposeString(
+                inputContext, keyEvent.key().sym());
+
+            // compose is invalid, ignore it.
+            if (!compose) {
+                return;
+            }
+
+            if (!compose->empty()) {
+                state->buffer_.type(*compose);
+            } else {
+                state->buffer_.type(Key::keySymToUnicode(keyEvent.key().sym()));
             }
             event.accept();
 
@@ -364,20 +399,31 @@ void Clipboard::trigger(InputContext *inputContext) {
     updateUI(inputContext);
 }
 void Clipboard::updateUI(InputContext *inputContext) {
+    auto *state = inputContext->propertyFor(&factory_);
     inputContext->inputPanel().reset();
+
+    const auto &search = state->buffer_.userInput();
+
+    Text preedit;
+    preedit.append(search);
+    if (!state->buffer_.empty()) {
+        preedit.setCursor(state->buffer_.cursorByChar());
+    }
+    inputContext->inputPanel().setPreedit(preedit);
 
     auto candidateList = std::make_unique<CommonCandidateList>();
     candidateList->setPageSize(instance_->globalConfig().defaultPageSize());
 
     // Append first item from history_.
     auto iter = history_.begin();
-    if (iter != history_.end()) {
+    if (iter != history_.end() &&
+        iter->text.find(search) != std::string::npos) {
         candidateList->append<ClipboardCandidateWord>(this, iter->text,
                                                       iter->passwordTimestamp);
         iter++;
     }
     // Append primary_, but check duplication first.
-    if (!primary_.empty()) {
+    if (!primary_.empty() && primary_.text.find(search) != std::string::npos) {
         if (!history_.contains(primary_)) {
             candidateList->append<ClipboardCandidateWord>(
                 this, primary_.text, primary_.passwordTimestamp);
@@ -388,13 +434,17 @@ void Clipboard::updateUI(InputContext *inputContext) {
         if (candidateList->totalSize() >= config_.numOfEntries.value()) {
             break;
         }
-        candidateList->append<ClipboardCandidateWord>(this, iter->text,
-                                                      iter->passwordTimestamp);
+        if (iter->text.find(search) != std::string::npos) {
+            candidateList->append<ClipboardCandidateWord>(
+                this, iter->text, iter->passwordTimestamp);
+        }
     }
     candidateList->setSelectionKey(selectionKeys_);
     candidateList->setLayoutHint(CandidateLayoutHint::Vertical);
 
-    Text auxUp(_("Clipboard (Press BackSpace/Delete to clear history):"));
+    Text auxUp(state->buffer_.empty()
+                   ? _("Clipboard (Press BackSpace/Delete to clear history):")
+                   : _("Search: "));
     if (!candidateList->totalSize()) {
         Text auxDown(_("No clipboard history."));
         inputContext->inputPanel().setAuxDown(auxDown);
