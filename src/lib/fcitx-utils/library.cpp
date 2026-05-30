2c7106f0f95fd6643b4c1f1a10eb220f590ced40
Weng Xuetian <wengxt@gmail.com>
2026-05-29 20:12:34 -0700

    Improve mmap error handling in library.cpp
    Handle mmap failure by checking if data is MAP_FAILED and setting it to nullptr.

--- src/lib/fcitx-utils/library.cpp
+++ src/lib/fcitx-utils/library.cpp
@@ -184,9 +184,13 @@ bool Library::findData(const char *slug, const char *magic, size_t lenOfMagic,
         data =
             mmap(nullptr, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd.fd(), 0);
         void *needunmap = nullptr;
-        needunmap = data;
+        if (data != MAP_FAILED) {
+            needunmap = data;
+        } else {
+            data = nullptr;
+        }
 #endif
-        if (data == MAP_FAILED) {
+        if (!data) {
             data = malloc(statbuf.st_size);
             needfree.reset(data);
             if (!data) {
